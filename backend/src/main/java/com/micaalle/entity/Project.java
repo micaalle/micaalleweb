1889c0181ff851f50ffda1a54b21d1cdde10a0a8
Michael <mbiskup2608@gmail.com>
2026-01-18 12:48:42 -0500

    Update Project.java

--- backend/src/main/java/com/micaalle/entity/Project.java
+++ backend/src/main/java/com/micaalle/entity/Project.java
@@ -4,28 +4,12 @@ import java.util.HashSet;
 import java.util.Set;
 
 import com.micaalle.util.DateFormat;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OrderBy;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
+import lombok.*;
 
 @Entity
 @Getter
@@ -35,33 +19,41 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name="projects", indexes = @Index(name = "date_order_ind", columnList = "date_order"))
 public class Project {
-    
+
+    public static final String PROJECT_ID_ERROR = "Invalid 'id', project not found.";
+    public static final String PROJECT_NAME_ERROR = "'name' should be between 3-30 characters.";
+    public static final String PROJECT_DESCRIPTION_ERROR = "'description' should be between 10-250 characters.";
+    public static final String PROJECT_START_DATE_ERROR = "'startDate' should be in format 'MM/YYYY'.";
+    public static final String PROJECT_END_DATE_ERROR = "'endDate' should be in format 'MM/YYYY'.";
+    public static final String PROJECT_LINK_ERROR = "'link' should be between 7-250 characters and start with 'http://' or 'https://'.";
+    public static final String PROJECT_PRESENT_ERROR = "'present' should be true or false.";
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(nullable = false)
-    @Size(min = 3, max = 30)
-    @NotBlank
+    @Size(min = 3, max = 30, message = PROJECT_NAME_ERROR)
+    @NotBlank(message = PROJECT_NAME_ERROR)
     private String name;
 
     @Column(nullable = false)
-    @Size(min = 10, max = 250)
-    @NotBlank
+    @Size(min = 10, max = 250, message = PROJECT_DESCRIPTION_ERROR)
+    @NotBlank(message = PROJECT_DESCRIPTION_ERROR)
     private String description;
 
     @Column(nullable = false)
-    @Pattern(regexp = "^(0[1-9]|1[0-2])/20[0-9]{2}$")
-    @NotBlank
+    @Pattern(regexp = "^(0[1-9]|1[0-2])/20[0-9]{2}$", message = PROJECT_START_DATE_ERROR)
+    @NotBlank(message = PROJECT_START_DATE_ERROR)
     private String startDate;
 
     @Column(nullable = false)
-    @Pattern(regexp = "^(0[1-9]|1[0-2])/20[0-9]{2}$")
-    @NotBlank
+    @Pattern(regexp = "^(0[1-9]|1[0-2])/20[0-9]{2}$", message = PROJECT_END_DATE_ERROR)
+    @NotBlank(message = PROJECT_END_DATE_ERROR)
     private String endDate;
 
     @Column(nullable = false)
-    @NotNull
+    @NotNull(message = PROJECT_PRESENT_ERROR)
     private Boolean present;
 
     @Column(nullable = false)
@@ -72,10 +64,14 @@ public class Project {
     private Set<Skill> skills = new HashSet<>();
 
     @Column(nullable = false)
-    @Size(min = 7, max = 250)
-    @Pattern(regexp = "^(http|https):\\/\\/(.*)$")
-    @NotBlank
+    @Size(min = 7, max = 250, message = PROJECT_LINK_ERROR)
+    @Pattern(regexp = "^(http|https):\\/\\/(.*)$", message = PROJECT_LINK_ERROR)
+    @NotBlank(message = PROJECT_LINK_ERROR)
     private String link;
+
+    // -------------------
+    // Methods
+    // -------------------
 
     public void createOrder() {
         String[] startSplit = this.startDate.split("/", 2);
@@ -103,4 +99,5 @@ public class Project {
         this.skills.remove(skill);
     }
 }
+
 
