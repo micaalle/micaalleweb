21aecd03813b603d5eb55277ba8f5643f0fc2cb7
Michael <mbiskup2608@gmail.com>
2026-01-20 14:12:33 -0500

    Update BackendApplication.java

--- backend/src/main/java/com/micaalle/backend/BackendApplication.java
+++ backend/src/main/java/com/micaalle/backend/BackendApplication.java
@@ -2,13 +2,11 @@ package com.micaalle.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@EntityScan(basePackages = "com.micaalle.entity")
-@SpringBootApplication(scanBasePackages = "com.micaalle")
+@SpringBootApplication
 public class BackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
-    }
-}
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
