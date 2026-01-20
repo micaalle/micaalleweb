d319473ac4293c6533b496b422826441fb3fc557
Michael <mbiskup2608@gmail.com>
2026-01-20 14:20:33 -0500

    Update BackendApplication.java

--- backend/src/main/java/com/micaalle/backend/BackendApplication.java
+++ backend/src/main/java/com/micaalle/backend/BackendApplication.java
@@ -3,7 +3,7 @@ package com.micaalle.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.micaalle")
 public class BackendApplication {
 
 	public static void main(String[] args) {
