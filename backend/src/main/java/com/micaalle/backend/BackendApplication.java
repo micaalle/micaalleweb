06ac4b73435969cfb0758b3bfeba6fcd45a289b3
Michael <mbiskup2608@gmail.com>
2026-01-20 14:24:36 -0500

    Update BackendApplication.java

--- backend/src/main/java/com/micaalle/backend/BackendApplication.java
+++ backend/src/main/java/com/micaalle/backend/BackendApplication.java
@@ -2,8 +2,13 @@ package com.micaalle.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com.micaalle")
+@SpringBootApplication
+@EnableJpaRepositories("com.micaalle.repository")
+#EntityScan("com.micaalle.entity")
+	
 public class BackendApplication {
 
 	public static void main(String[] args) {
