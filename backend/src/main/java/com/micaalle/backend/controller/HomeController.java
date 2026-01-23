fdcc5d79e997a4d61d73ef99511c871f77838907
Michael <mbiskup2608@gmail.com>
2026-01-23 15:12:36 -0500

    Update HomeController.java

--- backend/src/main/java/com/micaalle/backend/controller/HomeController.java
+++ backend/src/main/java/com/micaalle/backend/controller/HomeController.java
@@ -10,4 +10,24 @@ public class HomeController {
     public String home() {
         return "pages/home";
     }
+
+    @GetMapping("/about")
+    public String about() {
+        return "pages/about";
+    }
+
+    @GetMapping("/projects")
+    public String projects() {
+        return "pages/projects";
+    }
+
+    @GetMapping("/links")
+    public String links() {
+        return "pages/links";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "pages/contact";
+    }
 }
